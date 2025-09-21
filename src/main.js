5a6e9e344c871c29ee62364fb26f43e290064f25
SerhiiKutovyi <wagenrex@gmail.com>
2025-09-21 20:07:52 +0300

    added axios

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
 // import getImagesByQuery from './js/pixabay-api';
+
+import axios from 'axios';
+
+const API_KEY = '32386885-8dbf1bc36075d10a6eaf5580b';
+axios.defaults.baseURL = 'https://pixabay.com/';
+
+axios('/api', {
+  params: {
+    key: API_KEY,
+    q: 'car',
+    image_type: 'photo',
+  },
+})
+  .then(res => console.log(res.data.hits))
+  .catch(() => console.log('error'));
